4fb1afbd23f02a4ae53bce206091f5dfc3140d69
Gourav Agarwal <65907847+GouravGitHub1@users.noreply.github.com>
2021-07-01 14:06:34 +0530

    Update 15_maximumGap_sortArr.py

--- Array/15_maximumGap_sortArr.py
+++ Array/15_maximumGap_sortArr.py
@@ -1,6 +1,6 @@
 ## https://leetcode.com/problems/maximum-gap/submissions/
 
-## solution 1: direct approach
+## solution 1: direct approach : O(N logN)
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
         if (len(nums)) < 2:
@@ -15,4 +15,66 @@ class Solution:
                                 
         return maxDiff
       
-## solution 2: Optimized approach        
+## solution 2: Optimized approach using bucketization : O(N)  
+class Solution:
+    def maximumGap(self,nums : List[int]) -> int:
+        maxInp = max(nums)
+        minInp = min(nums)
+        lenInp = len(nums)
+        
+        if lenInp<2:
+            return 0
+        
+        ## optimal minimum max difference/gap in the list/Array
+        if (maxInp - minInp) % (lenInp - 1) == 0:
+            optimalGap = (maxInp - minInp) // (lenInp -1)
+        else:
+            optimalGap = (maxInp - minInp) // (lenInp - 1) + 1
+        
+        # print("maxInp",maxInp,"minInp",minInp,"lenInp",lenInp,"optimalGap",optimalGap)
+        
+        ## n(lenInp) buckets
+        maxArray = [float('-inf')]* lenInp
+        minArray = [float('inf')]* lenInp
+        
+        ## [1,1,1,1]
+        if optimalGap == 0:
+            return optimalGap
+        
+        # print(minArray, maxArray)
+        
+        ## put elements in buckets maintaining only min and max of a bucket
+        for el in nums:
+            elPos = (el -  minInp) // optimalGap
+            if (minArray[elPos] > el):
+                minArray[elPos] = el
+                
+            if (maxArray[elPos] < el):
+                maxArray[elPos] = el
+            
+        # print(minArray, maxArray)
+        
+        ## calculate the max difference between the buckets
+        maxDifference = float('-inf')
+        maxArrayIndex = 0
+        minArrayIndex = 1
+        
+        while(minArrayIndex < lenInp):
+            if (minArray[minArrayIndex] == float('inf')):
+                minArrayIndex += 1
+                continue
+            
+            if (maxArray[maxArrayIndex] == float('-inf')):
+                maxArrayIndex += 1
+                continue
+                
+            
+            if ( maxDifference < (minArray[minArrayIndex] - maxArray[maxArrayIndex]) ):
+                maxDifference = (minArray[minArrayIndex] - maxArray[maxArrayIndex])
+                
+            minArrayIndex += 1
+            maxArrayIndex += 1
+            
+
+        return maxDifference
+        
